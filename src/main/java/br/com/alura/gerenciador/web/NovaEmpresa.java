b6eb8988c493ce8ad4e99df6e74a3af06136c874
Alex Sandro <alexsrs@gmail.com>
2016-09-26 11:09:20 -0300

    atualizando
    Implementado filtro e inicado processo para login

--- src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -2,13 +2,11 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
@@ -23,6 +21,4 @@ public class NovaEmpresa extends HttpServlet {
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><body>Empresa adcionada com sucesso: </body></html>" + empresa.getNome());
 	}
-	
-	
 }
